26941fbc2dd2b10164b1a0e2fb5a6d0d27b2942a
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-08-23 02:36:32 +0000

    This reverts commit 5a8178438c9ec01f163dfae37ca54d7c90f1c948.

--- lib/components/button_comp.dart
+++ lib/components/button_comp.dart
@@ -2,31 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:link_opp/constants/colors.dart';
 
 class ButtonComp extends StatelessWidget {
-  final Function()? onTap;
-  const ButtonComp({super.key, required this.onTap});
+  const ButtonComp({super.key});
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: height * 0.02),
-        margin: EdgeInsets.symmetric(horizontal: width * 0.05),
-        decoration: BoxDecoration(
-          color: tdElevatedButton,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: const Center(
-          child: Text(
-            'Sign In',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
-            ),
+    
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: height * 0.02),
+      margin: EdgeInsets.symmetric(horizontal: width * 0.05),
+      decoration: BoxDecoration(
+        color: tdElevatedButton,
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: const Center(
+        child: Text(
+          'Sign In',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
