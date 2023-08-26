f1ef3a90c4962f0bc73c1c6db3a32156c861a789
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-08-26 04:19:57 +0000

    Added: Auth, Home and Firebase option, Modified: Main page and Pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:link_opp/auth/login_or_register.dart';
+import 'package:link_opp/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -22,4 +28,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
